a3a28ec14f7b87bed2bed0231bfbdc5451b0b72d
shvaich <260186867+shvaich@users.noreply.github.com>
2026-07-02 04:17:53 +0300

    added help subcommand

--- src/main/java/me/name/modtemplate/commands/CommandModTemplate.java
+++ src/main/java/me/name/modtemplate/commands/CommandModTemplate.java
@@ -1,8 +1,14 @@
 package me.name.modtemplate.commands;
 
+import me.name.modtemplate.ModTemplate;
 import me.name.modtemplate.config.ModTemplateConfig;
+import me.name.modtemplate.utils.ChatUtil;
 import me.name.modtemplate.utils.GuiUtil;
+import me.name.modtemplate.utils.StringUtil;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 
 import java.util.Collections;
 import java.util.List;
@@ -20,11 +26,23 @@ public class CommandModTemplate extends MyAbstractCommand {
 
     @Override
     protected void onCommand(ICommandSender sender, String[] args) {
+        if (args.length > 0 && isHelpSubcommand(StringUtil.toLowerCase(args[0]))) {
+            final String slashCommand = '/' + getCommandName();
+            final IChatComponent msg = new ChatComponentText(getHelpBar() + "\n" + getHelpHeader(ModTemplate.NAME + " Help"))
+                    .appendSibling(getHelpLine(slashCommand, "Opens the configuration GUI", "md"))
+                    .appendText("\n" + ChatUtil.centerLine(EnumChatFormatting.GRAY + "Some commands have a shortcut. Shortcuts are shown in parentheses after the description\n") + getHelpBar());
+
+            ChatUtil.addChatMessage(msg);
+            return;
+        }
         GuiUtil.openScreen(ModTemplateConfig.instance().getConfigGuiScreen());
     }
 
     @Override
     protected List<String> onTabComplete(ICommandSender sender, String[] args) {
+        if (args.length <= 1) {
+            return getListOfStringsMatchingLastWord(args, "help");
+        }
         return Collections.emptyList();
     }
 }
